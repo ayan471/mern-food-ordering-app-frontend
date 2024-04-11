8acdfd8ee93650e0f785b646484641184690bea0
Ayan Pakhira <pakhiraayan@gmail.com>
2024-04-11 12:32:00 +0530

    created checkouut

--- src/types.ts
+++ src/types.ts
@@ -15,7 +15,7 @@ export type MenuItem = {
 
 export type Restaurant = {
   _id: string;
-  suer: string;
+  user: string;
   restaurantName: string;
   city: string;
   country: string;
@@ -25,6 +25,34 @@ export type Restaurant = {
   menuItems: MenuItem[];
   imageUrl: string;
   lastUpdated: string;
+};
+
+export type OrderStatus =
+  | "placed"
+  | "paid"
+  | "inProgress"
+  | "outForDelivery"
+  | "delivered";
+
+export type Order = {
+  _id: string;
+  restaurant: Restaurant;
+  user: User;
+  cartItems: {
+    menuItemId: string;
+    name: string;
+    quantity: string;
+  }[];
+  deliveryDetails: {
+    name: string;
+    addressLine1: string;
+    city: string;
+    email: string;
+  };
+  totalAmount: number;
+  status: OrderStatus;
+  createdAt: string;
+  restaurantId: string;
 };
 
 export type RestaurantSearchResponse = {
